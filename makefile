0b75c7762931bb7b2864093a023a6f846bb67758
Max Hodak <maxhodak@gmail.com>
2010-04-11 17:17:56 -0400

    make install also creates a symbolic link for 'note' so either the singular or plural work now (i was instinctively trying to use the singular sometimes, so if that makes sense let's make it valid).

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 install:
 	cp notes.py /usr/bin/notes
 	chmod +x /usr/bin/notes
+	ln -fs /usr/bin/notes /usr/bin/note
--- notes.py
+++ notes.py
@@ -33,7 +33,6 @@ def main(argv=None):
   if argv[1] == 'new':
     path = "~/Documents/Notes/%s" % time.strftime("%Y/%m/%d")
     mkdir_p(path)
-    os.system("git add %s/%s.mdown" % (path, argv[2]))
     os.system("mate %s/%s.mdown" % (path, argv[2]))
   
   if argv[1] == 'search':
@@ -46,7 +45,7 @@ def main(argv=None):
     os.system("echo '.DS_Store' > ~/Documents/Notes/.gitignore")
     os.system("git init ~/Documents/Notes")
   
-  if argv[1] == 'git-add-all':
+  if argv[1] == 'add-all':
     os.system("cd ~/Documents/Notes && git add *")
   
   if argv[1] == 'commit':
