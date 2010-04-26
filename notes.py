c566943e2052ae689a274458a5282e6fee927533
Max Hodak <maxhodak@gmail.com>
2010-04-25 23:50:56 -0400

    made notes way more flexible and dynamic by checking env vars for things like notes-tree-root, username, editor, pager, and more.  added a delete method and a few more git methods, too.

--- notes.py
+++ notes.py
@@ -4,7 +4,7 @@
 A Python script for keeping track of Markdown-based notes.
 """
 
-import sys, os, time, errno, shutil, stat
+import sys, os, time, errno
 
 def normalize_argv(argv):
   if argv[0] is 'python':
@@ -24,6 +24,7 @@ def touch(fname, times = None):
     os.utime(fname, times)
 
 def display_help():
+  print "  Editor is %s, pager is %s." % (os.getenv('EDITOR'), os.getenv('PAGER'))
   print "  Valid commands:"
   print "    new <title>      Create a new note named <title> and open it in $EDITOR."
   print "    search <query>   Full text search for <query> in your notes tree."
@@ -32,42 +33,61 @@ def display_help():
   print "    git-init         (Re-)Initialize version control in your notes tree."
   print "    git-add          Add all untracked notes to version control."
   print "    git-commit       Commit the current state of your notes tree to version control."
+  print "    git-log          View your version control commit log in $PAGER."
+  print "    git-status       See the status of your notes tree with respect to unversioned changes."
   print "    help             Display this help message and quit."
+  print "  Notes is maintained by Max Hodak <max@myfit.com>.  Please report issues at http://github.com/maxhodak/notes/issues/."
 
 def main(argv=None):
   if argv is None:
     argv = normalize_argv(sys.argv)
+  
+  notespath = os.getenv("NOTESPATH")
+  if notespath is None:
+    notespath = "/Users/%s/Documents/Notes" % os.getlogin()
+    print "$NOTESPATH not set; setting default as %s" % notespath
+    print "You should add `export NOTESPATH=%s` (or otherwise) to your shell profile." % notespath
   
   if len(argv) < 2:
     display_help()
     sys.exit(2)
   
   if argv[1] == 'new':
-    path = "/Users/maxhodak/Documents/Notes/%s" % time.strftime("%Y/%m/%d")
+    path = "%s/%s" % (notespath,time.strftime("%Y/%m/%d"))
     mkdir_p(path)
     os.system("$EDITOR %s/%s.mdown" % (path, argv[2]))
   
   elif argv[1] == 'search':
-    os.system("grep -ir '%s' ~/Documents/Notes*" % argv[2])
+    os.system("grep -ir '%s' %s*" % (argv[2], notespath))
   
   elif argv[1] == 'list':
-    os.system("find ~/Documents/Notes -name '*.mdown' | cut -d '/' -f 9 | cut -d '.' -f 1")
+    os.system("find %s -name '*.mdown' | cut -d '/' -f 9 | cut -d '.' -f 1" % notespath)
   
   elif argv[1] == 'edit':
-    os.system("find ~/Documents/Notes -name '%s.mdown' -exec $EDITOR '{}' \;" % argv[2])
+    os.system("find %s -name '%s.mdown' -exec $EDITOR '{}' \;" % (notespath, argv[2]))
+  
+  elif argv[1] == 'delete':
+    check = raw_input("Delete %s: Are you sure? [y/N] " % argv[2])
+    if check in ("y","Y"):
+      os.system("find %s -name '%s.mdown' -exec rm '{}' \;" % (notespath, argv[2]))
   
   elif argv[1] == 'git-init':
-    os.system("echo '.DS_Store' > ~/Documents/Notes/.gitignore")
-    os.system("git init ~/Documents/Notes")
+    os.system("echo '.DS_Store' > %s/.gitignore" % notespath)
+    os.system("git init %s" % notespath)
   
   elif argv[1] == 'git-add':
-    os.system("cd /Users/maxhodak/Documents/Notes && git add *")
+    os.system("cd %s && git status --porcelain -uall | grep '^??' | cut -d" " -f2 | xargs git add" % \
+                notespath)
+  
+  elif argv[1] == 'git-log':
+    os.system("cd %s && git log" % notespath)
+  
+  elif argv[1] == 'git-status':
+    os.system("cd %s && git status -uall" % notespath)
   
   elif argv[1] == 'git-commit':
-    os.system("cd /Users/maxhodak/Documents/Notes && git commit -a")
-  
-  elif argv[1] == 'help':
-    display_help()
+    commit_msg = raw_input("Commit log message: ")
+    os.system("cd %s && git commit -am '%s'" % (notespath, commit_msg))
   
   else:
     display_help()
